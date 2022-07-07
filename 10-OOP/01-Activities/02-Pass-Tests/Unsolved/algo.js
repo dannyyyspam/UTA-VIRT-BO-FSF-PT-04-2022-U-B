d004ba0dadd600fdcf27ca4abd9aea6d4b05b43e
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-07-07 18:37:04 -0500

    add solved files

--- 10-OOP/01-Activities/02-Pass-Tests/Unsolved/algo.js
+++ 10-OOP/01-Activities/02-Pass-Tests/Unsolved/algo.js
@@ -3,27 +3,16 @@ function Algo() {}
 Algo.prototype.reverse = function(str) {
   // TODO: Your code here
   // TODO: convert string to an array
-
-  let result = str.split('').reverse().join("")
   // TODO: reverse substrings in array
   // TODO: convert array back into a string
-  return result
 };
 
 Algo.prototype.isPalindrome = function(str) {
-  if(str === this.reverse(str)){
-    return true
-  } else {
-    return false
-  }
+  // TODO: Your code here
 };
 
 Algo.prototype.capitalize = function(str) {
-  return str
-    .split(' ')
-    .map(word => {
-      return word.substring(0, 1).toUpperCase() + word.substring(1)
-    })
+  // TODO: Your code here
 };
 
 module.exports = Algo;

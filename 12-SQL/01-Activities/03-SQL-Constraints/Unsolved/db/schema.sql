36bb89033fd5e1588748f68c0c504756cb2951dd
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-07-21 20:47:01 -0500

    solve some files

--- 12-SQL/01-Activities/03-SQL-Constraints/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/03-SQL-Constraints/Unsolved/db/schema.sql
@@ -4,13 +4,15 @@ CREATE DATABASE inventory_db;
 USE inventory_db;
 
 CREATE TABLE prices (
-  id INTEGER NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   price DECIMAL(10,2)
 );
 
 CREATE TABLE books(
-  id INTEGER NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   book_name VARCHAR(30) NOT NULL,
   price INTEGER,
   in_stock BOOLEAN
+  FOREIGN KEY(price)
+  REFERENCES prices(id)
 );
--- 12-SQL/01-Activities/04-SQL-Join/Unsolved/db/query.sql
+++ 12-SQL/01-Activities/04-SQL-Join/Unsolved/db/query.sql
@@ -2,3 +2,4 @@ SELECT
   books.book_name AS book_name, prices.price AS price
 FROM books
 JOIN prices ON books.price = prices.id;
+ 

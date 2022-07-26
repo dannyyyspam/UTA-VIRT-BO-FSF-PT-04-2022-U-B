211cb1a1938d5cf659362929d80330f8b1bdf430
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-07-26 17:08:53 -0500

    add ORM files

--- 12-SQL/01-Activities/03-SQL-Constraints/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/03-SQL-Constraints/Unsolved/db/schema.sql
@@ -4,15 +4,13 @@ CREATE DATABASE inventory_db;
 USE inventory_db;
 
 CREATE TABLE prices (
-  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER NOT NULL,
   price DECIMAL(10,2)
 );
 
 CREATE TABLE books(
-  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER NOT NULL,
   book_name VARCHAR(30) NOT NULL,
   price INTEGER,
   in_stock BOOLEAN
-  FOREIGN KEY(price)
-  REFERENCES prices(id)
 );
